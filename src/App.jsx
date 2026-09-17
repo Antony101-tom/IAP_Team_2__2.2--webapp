23679db8129cd96b680189400e94ef4462eb7b72
Jade-192 <jadejosephine18@gmail.com>
2026-09-17 13:36:59 +0300

    Lab 5 test on frontend dont't get scared

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
 import UserDashboardPage from './pages/UserDashboardPage';
 import PharmacyDashboardPage from './pages/PharmacyDashboardPage';
+import MedicationsTable from './components/MedicationsTable';
 
 export default function App() {
   return (
+    <>
+    <MedicationsTable />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/dashboard/user" element={<UserDashboardPage />} />
       <Route path="/dashboard/pharmacy" element={<PharmacyDashboardPage />} />
     </Routes>
+  </>
   );
 }
